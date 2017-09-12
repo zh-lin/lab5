50e9e26f1f25099344de47c25f4b1a1437baa3ad
jack <papa830621@gmail.com>
2017-09-12 12:50:56 +0800

    code

---
+++ catkin_ws/src/me212arm/scripts/collision.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from planner import fk1, fk
+import numpy as np
+
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def in_collision(q, obstacle_segs):
+    arm_segments = [( (0,0), fk1(q) ), ( fk1(q), fk(q))]
+    
+    return intersect(arm_segments[0][0], arm_segments[0][1], obstacle_segs[0][0], obstacle_segs[0][1]) or intersect(arm_segments[1][0], arm_segments[1][1], obstacle_segs[0][0], obstacle_segs[0][1])
+    ## return True if there are segments from arm_segments and segments from obstacle_segs intersect
+
+if __name__=="__main__":
+    
+    obstacle_segs = [ [[0.0,0.0], [0.0,0.4]] ]  # line segs ((x1,z1)--(x2,z2))
+    print in_collision( [0,0], obstacle_segs)                       # False
+    print in_collision( [np.pi /4, -np.pi *3/4], obstacle_segs)     # True
+    print in_collision( [-0.45709828817786735, -1.4971869034039356], obstacle_segs) # False
